48d8364bc1a521144b8e13e7fd0d0dc9b66da116
Gabriel Cavalcante <gabrielcalk@outlook.com>
2021-10-27 17:06:24 -0400

    Setup all the models

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+//If has JawsDB add on, then new Sequilize will use the informations that they provide
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
