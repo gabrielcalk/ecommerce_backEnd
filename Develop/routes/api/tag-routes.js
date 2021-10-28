4cbb363dd2fbc9c9380e15447996dc619e76f62a
Gabriel Cavalcante <gabrielcalk@outlook.com>
2021-10-28 17:24:24 -0400

    done

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -26,16 +26,41 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/', async(req, res) => {
+  try{
+    const tag_post = await Tag.create(req.body)
+    res.status(200).json(tag_post)
+  }catch(err){
+    res.status(500).json(err)
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try{
+    const tag_update = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+    tag_update ? res.status(200).json(`id: ${req.params.id} has been update`)
+    : res.status(500).json('Could not find this id')
+  }catch(err){
+    res.status(500).json(err)
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try{
+    const tag_delete = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    tag_delete ? res.status(200).json(`id: ${req.params.id} has been deleter`)
+    : res.status(500).json('Could not find this id')
+  }catch(err){
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
